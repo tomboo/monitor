b739aea88814f774fe1c3f57909bf17d70c1d1a2
Thomas Booster <tom@Thomass-MacBook-Pro.local>
2018-09-15 06:28:15 -0700

    Part 2: Create a skeleton website

--- monitor/urls.py
+++ monitor/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('weight/', include('weight.urls')),
+    path('', RedirectView.as_view(url='/weight/')),
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
