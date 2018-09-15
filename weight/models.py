d4fb21e42af6cd41f8d1b66282f9ef4801990f45
Thomas Booster <tom@Thomass-MacBook-Pro.local>
2018-09-15 08:00:00 -0700

    Part 4: Django admin site

--- weight/models.py
+++ weight/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Weight(models.Model):
+    """A typical class defining a model, derived from the Model class."""
+
+    # Fields
+    date = models.DateTimeField(default=timezone.now)
+    weight = models.FloatField()
+    bodyfat = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Metadata
+    class Meta:
+        ordering = ['-date']
+
+    # Methods
+    def __str__(self):
+        return f'{self.date} {self.weight} {self.bodyfat} {self.user}'
