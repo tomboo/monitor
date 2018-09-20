6f6cf0ca9033b1f35ef840f265b0651cc54d3790
Tom Booster <tom.booster@gmail.com>
2018-09-20 07:55:17 -0700

    Initial integration of Highcharts

--- weight/urls.py
+++ weight/urls.py
@@ -4,4 +4,8 @@ from weight import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('weights/', views.weights, name='weights'),
+
+    path('json-example/', views.json_example, name='json_example'),
+    path('json-example/data/', views.chart_data, name='chart_data'),
 ]
--- weight/views.py
+++ weight/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from weight.models import Weight
+from django.contrib.auth.models import User
+
+from datetime import datetime
+
+
+def timestamp(date):
+    return int(datetime.combine(date, datetime.min.time()).timestamp()) * 1000
 
 
 def index(request):
@@ -17,3 +25,50 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+def weights(request):
+    user = User.objects.get(username='Tom')
+    dataset = Weight.objects\
+        .values('date', 'weight')\
+        .filter(user_id=user.id)\
+        .order_by('date')
+    print(dataset)
+    context = {
+        'dataset': dataset,
+    }
+    return render(request, 'weights.html', context=context)
+
+
+def json_example(request):
+    return render(request, 'json_example.html')
+
+
+def chart_data(request):
+    user = User.objects.get(username='Tom')
+    dataset = Weight.objects\
+        .values('date', 'weight')\
+        .filter(user_id=user.id)\
+        .order_by('date')
+    print(dataset)
+    for row in dataset:
+        print(timestamp(row['date']), row['weight'])
+
+    chart = {
+        'rangeSelector': {
+            'selected': 1
+        },
+        'title': {
+            'text': 'Weight Chart'
+        },
+        'series': [{
+            'name': 'Weight',
+            'tooltip': {
+                'valueDecimals': 2
+            },
+            'data': list(map(lambda row: [timestamp(row['date']), row['weight']], dataset))
+        }]
+    }
+    print(chart)
+
+    return JsonResponse(chart)
