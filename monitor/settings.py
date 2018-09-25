0038eab2d1d7edcf8cb51d51c16db7b2150398d6
Tom Booster <tom.booster@gmail.com>
2018-09-25 06:32:08 -0700

    Update ALLOWED_HOSTS with site and development server URL

--- monitor/settings.py
+++ monitor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tb-monitor.heroku.com', '127.0.0.1']
 
 
 # Application definition
